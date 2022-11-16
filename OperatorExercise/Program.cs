0c37d0c55e440e3bc5418e56506c4d3acfbbf541
leonardo oliveira <leonardo_olivesilva@icloud.com>
2022-11-15 18:17:15 -0800

    Finished Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+
+            
+
+            Console.WriteLine($"{a} / {b} is {div} remainder {mod}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = CalculateArea(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+            Console.ReadLine();
+
+        }
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
         }
     }
+
 }
